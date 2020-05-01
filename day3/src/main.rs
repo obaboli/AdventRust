a8116aa55d5b512df919346d5d5432f43bc50ee1
baboli <omeedbaboli@gmail.com>
2020-05-01 16:53:34 -0700

    day 3 part 1 cleanup

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,15 +1,15 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use std::collections::HashSet;
 
 struct Point {
-    xx: i64, 
+    xx: i64,
     yy: i64,
 }
 
 impl Point {
     fn up(&mut self) {
-        self.yy = self.yy.wrapping_sub(1);
+        self.yy -= 1;
     }
     fn down(&mut self) {
         self.yy += 1;
@@ -18,7 +18,7 @@ impl Point {
         self.xx += 1;
     }
     fn left(&mut self) {
-       self.xx = self.xx.wrapping_sub(1);
+        self.xx -= 1
     }
 }
 
@@ -28,14 +28,14 @@ fn main() -> io::Result<()> {
     let mut hash1 = HashSet::new();
     let mut hash2 = HashSet::new();
 
-    let mut point = Point { xx: 0, yy: 0 }; 
+    let mut point = Point { xx: 0, yy: 0 };
 
     for word in lines.next().unwrap().unwrap().split(',') {
         //println!("{} ", word);
         let (d, n) = word.split_at(1);
         let n = n.parse::<u64>().unwrap();
         let d = d.chars().next().unwrap();
-        
+
         for _ in 0..n {
             match d {
                 'R' => point.right(),
@@ -45,17 +45,17 @@ fn main() -> io::Result<()> {
                 _ => panic!("Invalid Direction; {}", d),
             }
             hash1.insert((point.xx, point.yy));
-        } 
+        }
     }
-    
-    let mut point = Point { xx: 0, yy: 0 }; 
+
+    let mut point = Point { xx: 0, yy: 0 };
 
     for word in lines.next().unwrap().unwrap().split(',') {
         //println!("{} ", word);
         let (d, n) = word.split_at(1);
         let n = n.parse::<u64>().unwrap();
         let d = d.chars().next().unwrap();
-        
+
         for _ in 0..n {
             match d {
                 'R' => point.right(),
@@ -65,15 +65,15 @@ fn main() -> io::Result<()> {
                 _ => panic!("Invalid Direction; {}", d),
             }
             hash2.insert((point.xx, point.yy));
-        } 
+        }
     }
 
     //println!("Intersection: {:?}", hash1.intersection(&hash2).collect::<Vec<&(i64,i64)>>());
-    let intersections = hash1.intersection(&hash2).collect::<Vec<&(i64,i64)>>();
-    let mut min = u32::MAX;
+    let intersections = hash1.intersection(&hash2).collect::<Vec<&(i64, i64)>>();
+    let mut min = u64::MAX;
     for i in intersections {
-        let c_min: u32 = (i.0 - 0).abs() as u32 + (i.1 - 0).abs() as u32;
-        
+        let c_min: u64 = (i.0 - 0).abs() as u64 + (i.1 - 0).abs() as u64;
+
         if min > c_min {
             min = c_min
         }
